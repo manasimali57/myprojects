db31e8d0b2c3e1c6d37ea1e310d4636d67a3522d
manasimali57 <manasimali246@gmail.com>
2024-09-08 22:37:37 +0530

    Add files via upload

---
+++ lovebtn.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "I luv you moreee";
+    gif.src = "https://i.pinimg.com/originals/d4/45/bc/d445bc4afd37c311adde4d127bee2e31.gif";  
+});
+
+noBtn.addEventListener("mouseover", () => {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+});
